27e199252fe9186dcec115cf184ade6ae71a734f
Fernanda Alves <fernandaalves.tds2.senai@gmail.com>
2025-04-15 11:45:21 -0300

    style: update colors and background

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,14 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <h1 className={styles.title}>Anime List</h1>
-        <p className={styles.description}>Um projeto para listar animes</p>
+        <h1 className={styles.title}>⭐Anime List⭐</h1>
       </header>
 
       <main className={styles.main}>
         <AnimeList />
       </main>
-      
+
     </div>
   );
 }
